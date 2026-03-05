bffc8d8e5c72bf9f227f29aadb913e4df9ca1aa4
agniv-sg <85630225+agniv-sg@users.noreply.github.com>
2026-03-05 07:49:55 +0000

    Deploying to gh-pages from @ agniv-sg/agniv-sg.github.io@8c0939ce21876f10e9a127e00c38e66c4552f674 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -495,7 +495,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=WrAktq0AAAAJ", "_blank");
         },
       },{
         id: 'social-orcid',
